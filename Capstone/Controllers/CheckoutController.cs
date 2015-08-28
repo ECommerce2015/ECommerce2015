ad5391a5f102048f2e125bb8264fa16afd17f8c3
Jack Rola <jrola@precisiondesignstudios.com>
2015-08-28 18:59:14 -0400

    added a checkout pageand styles

--- Capstone/Controllers/CheckoutController.cs
+++ Capstone/Controllers/CheckoutController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capstone.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,6 +20,24 @@ namespace Capstone.Controllers
         public ActionResult Checkout()
         {
             return View();
+        }
+        private int isExisting(int product_id)
+        {
+            List<Item> cart = (List<Item>)Session["cart"];
+            for (int i = 0; i < cart.Count; i++)
+
+                if (cart[i].Pr.product_ID == product_id)
+                    return i;
+            return -1;
+
+        }
+        public ActionResult Delete(int product_id, int category_id)
+        {
+            int index = isExisting(product_id);
+            List<Item> cart = (List<Item>)Session["cart"];
+            cart.RemoveAt(index);
+            Session["cart"] = cart;
+            return RedirectToAction("Checkout", "Checkout", new { category_ID = category_id });
         }
     }
 }
